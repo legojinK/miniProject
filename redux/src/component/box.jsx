7a3410bf6a2916332d2e4c40ae20621fd43fee1a
heejinkim <hjkim2@okestro.com>
2024-08-25 22:14:37 +0900

    [feat] redux 테스트

--- redux/src/component/box.jsx
+++ redux/src/component/box.jsx
@@ -5,7 +5,7 @@ const Box = () => {
     const count = useSelector(state=>state.count)
     return (
         <div>
-            <h3>자식 컴포넌트 : {count}</h3>
+            <h3>자식 컴포넌트 : {count} </h3>
         </div>
     );
 };
